064fbda9c781285aa17262fac7341da8c6cb18ba
cqc <2297436146@qq.com>
2018-12-17 20:36:16 +0800

    IDEA结合git的第二次使用

--- git-test/src/com/itcast/git/demo01.java
+++ git-test/src/com/itcast/git/demo01.java
@@ -9,4 +9,10 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println("hello,git");
     }
+
+    public void test(){
+        int i=0;
+        i+=10;
+        System.out.println(i);
+    }
 }
