58154d0a986de589296131cd66aec74226d12e73
cqc <2297436146@qq.com>
2018-12-17 20:21:12 +0800

    IDEA结合git的第一次使用

---
+++ git-test/src/com/itcast/git/demo01.java
@@ -0,0 +1,12 @@
+package com.itcast.git;
+
+/**
+ * @program: git-test
+ * @author: Mr.Cai
+ * @create: 2018-12-17 20:07
+ **/
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
